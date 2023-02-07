7d494a61bfeb461ad049ed11ae60957522d9c9c9
LincKim <lincolnkimutai@gmail.com>
2023-02-07 11:44:25 +0300

    Learning Ruby basics

--- main.rb
+++ main.rb
@@ -2,36 +2,55 @@
 
 # 1.0 Printing Methods
 # TODO: puts
+puts 'Hello Ruby'
 
 # TODO: print
+print 'Hola Ruby'
 
-# TODO: pp
+# TODO: pp (Pretty Print)- used for arrays and hashes (objects in JS)
+pp [1,2,3]
 
 # 2.0 Strings
 # TODO: CREATE A STRING
-
+# you dont have to use the variable keyword like in js.
+# if the name of the variable is in lowercase then it is as though you have used "let" in js
+#If it is in UPPPERCASE then it is a though you have used "const"
+last_name = 'Doe'
+FIRST_NAME = 'Jane'
 # TODO: CREATE A STRING WITH SINGLE QUOTES
 
 # TODO: INTERPOLATE STRINGS
-
+full_name = "First Name is #{FIRST_NAME}. Last Name is #{last_name}."
 # TODO: String methods (upcase, downcase, capitalize)
+puts full_name.upcase
+puts full_name
+puts full_name.downcase
+
+puts full_name.capitalize
 
 # 3.0 Numbers
 # TODO: Integers
+height = 100
 
 # TODO: Float
+temp = 55.5
 
 # TODO: Number methods (floor, ceil, to_i, to_f)
-
+puts temp.floor
+puts temp.ceil
+puts temp.to_i
+puts temp.to_f
 
 # 4.0 Boolean
-# TODO: True / Truthy
-
-# TODO: False / Falsy
+# TODO: True / Truthy - all real world values are truthy
+is_tuesday = true
+# TODO: False / Falsy - 
+is_wednesday = false
 
 # 5.0 Symbols
 # TODO: Create symbol
-
+:green
+puts :green
 
 # 6.0 Arrays
 # TODO: Create array natively
